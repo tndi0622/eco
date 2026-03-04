cc2a7e2c11a02ccb24d27cd25d63a8024e6ce722
tndi0622 <tndi0622@gmail.com>
2026-03-04 11:45:04 +0900

    modify

--- supabase/functions/send-push/index.ts
+++ supabase/functions/send-push/index.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 import { JWT } from "https://esm.sh/google-auth-library@9"
 
-serve(async (req) => {
+serve(async (req: Request) => {
     const supabase = createClient(
         Deno.env.get('SUPABASE_URL') ?? '',
         Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
